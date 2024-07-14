9d1ab2444c24e19d8e2aa513e47d77495fc94407
binhnguyen96html <binhnguyenbiz96@gmail.com>
2024-07-14 01:20:18 -0700

    edited database setting

--- todos/settings.py
+++ todos/settings.py
@@ -74,16 +74,25 @@ WSGI_APPLICATION = 'todos.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'TodoDB',
+#         'USER': 'postgres',
+#         'PASSWORD': "112233",
+#         'HOST': 'localhost',
+#         'PORT': "5433"
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'TodoDB',
-        'USER': 'postgres',
-        'PASSWORD': "112233",
-        'HOST': 'localhost',
-        'PORT': "5433"
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+
 
 
 # Password validation
